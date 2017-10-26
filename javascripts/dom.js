bb98fcd2356ca773947b015e5b652c189ad99f45
Jess Brawner <jessnbillbrawner@gmail.com>
2017-10-25 20:49:02 -0500

    for forecastData: formatted date info, removed current day from forecast array, and grouped forecast by day

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -2,17 +2,19 @@
 
 const displayCurrentConditions = (currentWeather) => {
 	let domString = `
-						<div class="card col-md-3 col-mid-offset-4.5" id="current-card">
-							<div class="day">
-								<h2>Current Conditions</h2>
-							</div>
-							<div class="thumbnail">
-								<img src="http://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png" alt="">
-							</div>
-							<h3 id="current-temp">${currentWeather.main.temp}&#176 F</h3>
-							<h4 id="current-condition">${currentWeather.weather[0].description}</h4>
-							<h4 id="current-pressure">Air Pressure: ${currentWeather.main.pressure} hPa</h4>
-							<h4 id="current-wind">Wind Speed: ${currentWeather.wind.speed} mph</h4>`;
+							<div class="card col-md-3 col-mid-offset-4.5" id="current-card">
+								<div class="day">			
+									<h2 id="city">${currentWeather.name}</h2>
+									<h2>Current Conditions</h2>
+								</div>
+								<div class="thumbnail">
+									<img src="http://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png" alt="">
+								</div>
+								<h3 id="current-temp">${currentWeather.main.temp}&#176 F</h3>
+								<h4 id="current-condition">${currentWeather.weather[0].description}</h4>
+								<h4 id="current-pressure">Air Pressure: ${currentWeather.main.pressure} hPa</h4>
+								<h4 id="current-wind">Wind Speed: ${currentWeather.wind.speed} mph</h4>
+							</div>`;
 					
 
 	printCurrent(domString);
@@ -22,20 +24,20 @@ const printCurrent = (strang) => {
 	$('.current-condition').append(strang);
 };
 
-const displayForecast = (weatherByDayArray) => {
+const displayForecast = (resultDays) => {
 	let forecastString = '';
-	for(let i = 0; i < weatherByDayArray.length; i++){
+	for(let i = 0; i < resultDays.length; i++){
 		forecastString += `
 						<div class="card" id="forecast-card">
 							<div class="day">
-								<h2>${weatherByDayArray[i].dayObject.day}, ${weatherByDayArray[i].dayObject.hour} </h2>
+								<h2>${resultDays[i].dayOfTheWeek}</h2>
 							</div>
 							<div class="thumbnail">
-								<img src="http://openweathermap.org/img/w/${weatherByDayArray[i].dayObject.icon}.png" alt="">
+								<img src="http://openweathermap.org/img/w/${resultDays[i].icon}.png" alt="">
 							</div>
-							<h3 id="temp">${weatherByDayArray[i].dayObject.temp}&#176 F</h3>
-							<h4 id="forecast-condition">${weatherByDayArray[i].dayObject.description}</h4>
-							<h4 id="current-wind">Wind Speed: ${weatherByDayArray[i].dayObject.wind} mph</h4>
+							<h3 id="temp">${resultDays[i].main.temp}&#176 F</h3>
+							<h4 id="forecast-condition">${resultDays[i].weather.description}</h4>
+							<h4 id="current-wind">Wind Speed: ${resultDays[i].wind.speed} mph</h4>
 						</div`;
 					}
 	printForecast(forecastString);
